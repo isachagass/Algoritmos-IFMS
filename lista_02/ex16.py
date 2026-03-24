49509557b26abd99318c485fa7be6f9f4fbf0e2c
Isabelli C. Kubo <163148600+isachagass@users.noreply.github.com>
2026-03-24 19:04:37 -0400

    Refactor ticket calculation code

--- lista_02/ex16.py
+++ lista_02/ex16.py
@@ -13,19 +13,4 @@ if str.lower(tipo) == 'unitário':
         quant += 1
     print(f"Qunatidade de bilhetes: {quant}\nTroco: {resto}")
 
-//ex17.py
 
-numeros = list(map(int, input("Digite 3 valores: ").split(',')))
-
-
-if numeros[0] < numeros[1] + numeros[2]:
-    print("Triângulo válido")
-    if numeros[0] ==  numeros[1] == numeros[2]:
-        print("Triângulo esquilatero")
-    elif numeros[0] == numeros[1] or numeros[1] == numeros[2] or numeros[0] == numeros[2]:
-        print("Triângulo isósceles")
-    else:
-        print("Triangulo escaleno")
-
-else:
-    print("Não é um triangulo")
